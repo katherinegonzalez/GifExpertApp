3de569ea3792aee5b8603819579a3051ca3a3b43
katherinex <kathexio@gmail.com>
2021-02-28 20:22:11 -0500

    replace http by https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
         
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category)}&limit=10&api_key=ehCxuH2BVdfrlfRwY0km2tRg2nwAw4LJ`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category)}&limit=10&api_key=ehCxuH2BVdfrlfRwY0km2tRg2nwAw4LJ`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
