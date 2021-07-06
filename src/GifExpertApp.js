fbe103b76fadb309d90de6ec87eda48653eb553d
katherinex <kathexio@gmail.com>
2021-07-05 21:45:00 -0500

    Unit tests GifExpertApp component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
-const GifExpertApp = () => {
+const GifExpertApp = ({ defaultCategories = [] }) => {
 
     // const categories = ['One Punch', 'Samurai X', 'Dragon Ball Z'];
 
-    const [categories, setCategories] = useState(['One Punch']);
+    const [categories, setCategories] = useState(defaultCategories);
 
     /*const handleAdd = () => {
         // setCategories([...categories, 'HunterXHunter']); ó también se puede:
--- src/tests/GifExpertApp.test.js
+++ src/tests/GifExpertApp.test.js
@@ -7,5 +7,12 @@ describe('Pruebas en <GifExpertApp />', () => {
         const wrapper = shallow( <GifExpertApp /> );
         expect( wrapper ).toMatchSnapshot();
     });
+
+    test('Debe mostrar una lista de categorias', () => {
+        const categories = ['One Punch', 'Dragon Ball'];
+        const wrapper = shallow( <GifExpertApp defaultCategories={categories} /> );
+        expect( wrapper ).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(categories.length);
+    });
     
 })
